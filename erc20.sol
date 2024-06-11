ab9437baa237ffb1eb9694420066f7268c681744
Metacrafters0x1 <149813536+Metacrafters0x1@users.noreply.github.com>
2024-06-11 10:01:45 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+contract firstcontract {
+
+    string public name;
+    string public symbol;
+    uint256 public totalSupply=0;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        name = "cu summer internship";
+        symbol = "CUST";
+    }
+
+    function mint(address account, uint256 amount) public {
+        totalSupply += amount;
+        balances[account] += amount;
+    }
+
+    function burn(address account, uint256 amount) public {    
+        require(balances[account] >= amount, "Sorry low balance than requested");
+        totalSupply -= amount;
+        balances[account] -= amount;
+    }
+}
